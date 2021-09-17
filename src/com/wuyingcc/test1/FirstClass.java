f5dd58fe5c25e5c3af90a4e0b79da9c6c863fe9a
wuyingcc <wuyingcc@outlook.com>
2021-09-17 21:53:46 +0800

    tijao1

---
+++ src/com/wuyingcc/test1/FirstClass.java
@@ -0,0 +1,15 @@
+package com.wuyingcc.test1;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hahahah");
+        System.out.println("hahahah");
+        System.out.println("hahahah");
+        System.out.println("hahahah");
+        System.out.println("hahahah");
+        System.out.println("hahahah");
+        System.out.println("hahahah");
+        System.out.println("hahahah");
+        System.out.println("hahahah");
+    }
+}
